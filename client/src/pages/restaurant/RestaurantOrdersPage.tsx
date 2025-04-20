508c8eaca6d70db6c050487ccecdc509e7102cca
Shereen Punnassery <pshereen@gmail.com>
2025-04-19 18:39:42 -0700

    Fix Build error

--- client/src/pages/restaurant/RestaurantOrdersPage.tsx
+++ client/src/pages/restaurant/RestaurantOrdersPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import DashboardLayout from '../../components/DashboardLayout.bak';
 import { useSelector } from 'react-redux';
