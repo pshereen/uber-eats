0d3d9e6a8aa592045845c9844f6c9d5e852cdc85
Shereen Punnassery <pshereen@gmail.com>
2025-04-20 11:03:14 -0700

    Fix CORS for frontend hosted on GitHub Pages

--- server/index.ts
+++ server/index.ts
@@ -16,25 +16,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(function (req, res, next) {
-  const allowedOrigins = ['https://pshereen.github.io'];
-  const origin = req.headers.origin as string | undefined;
-
-  if (origin && allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); // must be a string
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, UPDATE');
-
-  next();
-});
-
+app.use(cors());
 
 app.use(express.json());
 
